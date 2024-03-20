81e7669d64cd917a32163bd47a87e601114a52d6
Daniel J Kunz <djk44@cam.ac.uk>
2024-03-20 09:48:47 +0000

    Add logo

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
 html_css_files = ["css/custom.css"]
-html_logo = "_static/img/squidpy_horizontal.png"
+html_logo = "_static/img/donorpy_logo.svg"
 
 html_title = project_name
 
