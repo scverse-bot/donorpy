4e5128bad4827c375f0374e6bffe4590ac8f7304
Daniel J Kunz <djk44@cam.ac.uk>
2024-03-29 09:03:07 +0000

    Test parallelised functions.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pytest
 
 import donorpy as dn
+from donorpy.core import _emd_donorpair_pmt
 
 
 def test_package_has_version():
@@ -31,7 +32,7 @@ def test_emd_identical():
 
     adata = ad.AnnData(obs=metadata, obsm={"X_diffmap": X_diffmap})
 
-    df_emd = df_emd = dn.emd_pval(
+    df_emd = dn.emd_pval(
         adata,
         obs_donorID="donorID",
         basis="X_diffmap",
@@ -41,3 +42,29 @@ def test_emd_identical():
     )
     assert all(df_emd.emd == 0.0)
     assert all(df_emd.pval == 1.0)
+
+
+def test_emd_donorpair_pmt():
+    """Tests that EMD is 0 when comparing two identical donors. Tests function that is parallelised."""
+
+    n_cells_donor = 50
+    n_comp = 3
+    metadata = pd.DataFrame(
+        data={"donorID": np.repeat(["donor_1", "donor_2"], n_cells_donor)},
+        index=[str(x) for x in range(n_cells_donor * 2)],
+    )
+    rng = np.random.default_rng()
+    dist_1 = rng.random((n_cells_donor, n_comp))
+    dist_2 = dist_1
+    X_diffmap = np.concatenate((dist_1, dist_2), axis=0)
+
+    adata = ad.AnnData(obs=metadata, obsm={"X_diffmap": X_diffmap})
+
+    array_donor_EMD = _emd_donorpair_pmt(
+        adata,
+        ["donor_1", "donor_2"],
+        n_comp=2,
+        n_pmt_pval=5,
+    )
+
+    assert all(array_donor_EMD[-1, :] == 0.0)
